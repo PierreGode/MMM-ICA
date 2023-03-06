f668f63392283bcaf405a9337d574af9457590b7
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-03-06 22:12:11 +0100

    Update MMM-ICA.js

--- MMM-ICA.js
+++ MMM-ICA.js
@@ -1,12 +1,18 @@
-/* global Module */
-
 Module.register("MMM-ICA", {
   defaults: {
     username: "",
     password: "",
     apiUrl: "",
-    updateInterval: 60 * 60 * 1000, // Update every hour.
-    retryDelay: 5 * 60 * 1000 // Retry every 5 minutes if an error occurs.
+    storeApiUrl: "",
+    updateInterval: 60 * 60 * 1000,
+    retryDelay: 5 * 60 * 1000,
+    settings: {
+      apiEndpoints: [
+        { name: "stores", enabled: true },
+        { name: "minbonus", enabled: true },
+        { name: "offers", enabled: true, storeId: "11111" }
+      ]
+    }
   },
 
   start: function() {
@@ -22,7 +28,6 @@ Module.register("MMM-ICA", {
 
     this.sendSocketNotification("GET_AUTH_TICKET", this.config);
   },
-
   getDom: function() {
     const wrapper = document.createElement("div");
     wrapper.className = "small bright";
@@ -36,7 +41,6 @@ Module.register("MMM-ICA", {
     return wrapper;
   },
 
-  // Override socket notification handler.
   socketNotificationReceived: function(notification, payload) {
     console.log("Received socket notification:", notification, "with payload:", payload);
 
@@ -65,7 +69,6 @@ Module.register("MMM-ICA", {
       console.log(`Got authentication ticket: ${authTicket}`);
       this.authTicket = authTicket;
       this.updateDom();
-
       // Schedule the first call to the card accounts API.
       setTimeout(() => {
         this.getCardAccounts();
@@ -96,6 +99,74 @@ Module.register("MMM-ICA", {
       setTimeout(() => {
         this.getCardAccounts();
       }, this.config.updateInterval);
+    } else if (notification === "STORES_RESULT") {
+      if (payload.error) {
+        console.error(`Error getting stores: ${payload.error}`);
+        setTimeout(() => {
+          this.getStores();
+        }, this.config.retryDelay);
+       return;
+      }
+
+      console.log(`Got stores: ${JSON.stringify(stores)}`);
+      this.stores = stores;
+      this.updateDom();
+      // Schedule the next call to the stores API.
+      setTimeout(() => {
+        this.getStores();
+      }, this.config.updateInterval);
+    } else if (notification === "MIN_BONUS_RESULT") {
+      if (payload.error) {
+        console.error(`Error getting min bonus: ${payload.error}`);
+        setTimeout(() => {
+          this.getMinBonus();
+        }, this.config.retryDelay);
+        return;
+      }
+
+      const minBonus = payload.minBonus;
+      if (!minBonus) {
+        console.error("Error: Unable to retrieve min bonus.");
+        setTimeout(() => {
+          this.getMinBonus();
+        }, this.config.retryDelay);
+        return;
+      }
+
+      console.log(`Got min bonus: ${minBonus}`);
+      this.minBonus = minBonus;
+      this.updateDom();
+
+      // Schedule the next call to the min bonus API.
+      setTimeout(() => {
+        this.getMinBonus();
+      }, this.config.updateInterval);
+    } else if (notification === "OFFERS_RESULT") {
+      if (payload.error) {
+        console.error(`Error getting offers: ${payload.error}`);
+        setTimeout(() => {
+          this.getOffers();
+        }, this.config.retryDelay);
+        return;
+      }
+
+      const offers = payload.offers;
+      if (!offers) {
+        console.error("Error: Unable to retrieve offers.");
+        setTimeout(() => {
+          this.getOffers();
+        }, this.config.retryDelay);
+        return;
+      }
+
+      console.log(`Got offers: ${JSON.stringify(offers)}`);
+      this.offers = offers;
+      this.updateDom();
+
+      // Schedule the next call to the offers API.
+      setTimeout(() => {
+        this.getOffers();
+      }, this.config.updateInterval);
     } else {
       console.warn(`Unknown socket notification received: ${notification}`);
     }
@@ -113,5 +184,52 @@ Module.register("MMM-ICA", {
     };
 
     this.sendSocketNotification("GET_CARD_ACCOUNTS", options);
+  },
+
+  getStores: function() {
+    console.log("Retrieving stores");
+
+    const options = {
+      method: "GET",
+      url: `${this.config.storeApiUrl}/user/stores`,
+      headers: {
+        "AuthenticationTicket": this.authTicket
+      }
+    };
+
+    this.sendSocketNotification("GET_STORES", options);
+  },
+
+  getMinBonus: function() {
+    console.log("Retrieving min bonus");
+
+    const options = {
+      method: "GET",
+      url: `${this.config.apiUrl}/user/minbonustransaction`,
+      headers: {
+        "AuthenticationTicket": this.authTicket
+      }
+    };
+
+    this.sendSocketNotification("GET_MIN_BONUS", options);
+  },
+
+  getOffers: function() {
+    console.log("Retrieving offers");
+
+    let url = `${this.config.apiUrl}/offers`;
+    if (this.config.settings.apiEndpoints.offers.storeId) {
+      url += `?Stores=${this.config.settings.apiEndpoints.offers.storeId}`;
+    }
+
+    const options = {
+      method: "GET",
+      url: url,
+      headers: {
+        "AuthenticationTicket": this.authTicket
+      }
+    };
+
+    this.sendSocketNotification("GET_OFFERS", options);
   }
-});
+}); 
