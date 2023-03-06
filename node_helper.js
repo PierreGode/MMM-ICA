aa250b26aa60d856f38dc0432fd7240f0220ab34
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-03-06 18:27:17 +0100

    Create node_helper.js

---
+++ node_helper.js
@@ -0,0 +1,73 @@
+const NodeHelper = require("node_helper");
+const request = require("request");
+
+module.exports = NodeHelper.create({
+  start: function() {
+    console.log(`Starting helper: ${this.name}`);
+  },
+
+  socketNotificationReceived: function(notification, payload) {
+    console.log("Received socket notification:", notification, "with payload:", payload);
+
+    if (notification === "GET_AUTH_TICKET") {
+      this.config = payload;
+      console.log("Retrieving authentication ticket");
+
+      const authHeader = `Basic ${Buffer.from(`${payload.username}:${payload.password}`).toString("base64")}`;
+      const options = {
+        method: "GET",
+        url: `${payload.apiUrl}/login`,
+        headers: {
+          "Authorization": authHeader
+        }
+      };
+
+      this.makeRequest(options);
+    }
+  },
+
+  makeRequest: function(options) {
+    var self = this;
+    request(options, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        const authTicket = response.headers["authenticationticket"];
+        console.log(response.headers); // Add this line
+        if (!authTicket) {
+          console.error("Error: Unable to retrieve authentication ticket.");
+          self.sendSocketNotification("AUTH_TICKET_RESULT", { error: "Unable to retrieve authentication ticket." });
+          return;
+        }
+
+        console.log(`Got authentication ticket: ${authTicket}`);
+        self.authTicket = authTicket;
+
+        const cardAccountsOptions = {
+          method: "GET",
+          url: `${self.config.apiUrl}/user/cardaccounts`,
+          headers: {
+            "AuthenticationTicket": authTicket
+          }
+        };
+
+        self.makeCardAccountsRequest(cardAccountsOptions);
+      } else {
+        console.error(`Error getting authentication ticket: ${error}`);
+        self.sendSocketNotification("AUTH_TICKET_RESULT", { error: error });
+      }
+    });
+  },
+  
+  makeCardAccountsRequest: function(options) {
+    var self = this;
+    request(options, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        const cardAccounts = JSON.parse(body);
+        console.log("Got card accounts:", cardAccounts);
+        self.sendSocketNotification("CARD_ACCOUNTS_RESULT", { cardAccounts: cardAccounts });
+      } else {
+        console.error(`Error getting card accounts: ${error}`);
+        self.sendSocketNotification("CARD_ACCOUNTS_RESULT", { error: error });
+      }
+    });
+  }
+});
