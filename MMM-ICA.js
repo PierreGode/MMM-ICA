bbb3257c24ef62bdb0605da5cfc8028f7d3780ba
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-03-06 18:26:02 +0100

    Create MMM-ICA.js

---
+++ MMM-ICA.js
@@ -0,0 +1,115 @@
+Module.register("MMM-ICA", {
+  defaults: {
+    username: "",
+    password: "",
+    apiUrl: "",
+    updateInterval: 60 * 60 * 1000, // Update every hour.
+    retryDelay: 5 * 60 * 1000 // Retry every 5 minutes if an error occurs.
+  },
+
+  start: function() {
+    console.log("Module config:", this.config);
+    Log.info(`Starting module: ${this.name}`);
+
+    if (!this.config.username || !this.config.password) {
+      console.error("Error: username or password not provided in module config.");
+      this.authTicket = "";
+      this.updateDom();
+      return;
+    }
+
+    this.sendSocketNotification("GET_AUTH_TICKET", this.config);
+  },
+
+getDom: function() {
+    const wrapper = document.createElement("div");
+    wrapper.className = "small bright";
+
+    if (this.cardAccounts) {
+      wrapper.innerHTML = `Saldo: ${this.cardAccounts.Cards[0].Accounts[0].Balance}`;
+    } else {
+      wrapper.innerHTML = "Waiting for card accounts...";
+    }
+
+    return wrapper;
+  },
+
+  // Override socket notification handler.
+  socketNotificationReceived: function(notification, payload) {
+    console.log("Received socket notification:", notification, "with payload:", payload);
+
+    if (notification === "AUTH_TICKET_RESULT") {
+      if (payload.error) {
+        console.error(`Error getting authentication ticket: ${payload.error}`);
+        this.authTicket = "";
+        this.updateDom();
+        setTimeout(() => {
+          this.sendSocketNotification("GET_AUTH_TICKET", this.config);
+        }, this.config.retryDelay);
+        return;
+      }
+
+      const authTicket = payload.authTicket;
+      if (!authTicket) {
+        console.error("Error: Unable to retrieve authentication ticket.");
+        this.authTicket = "";
+        this.updateDom();
+        setTimeout(() => {
+          this.sendSocketNotification("GET_AUTH_TICKET", this.config);
+        }, this.config.retryDelay);
+        return;
+      }
+
+      console.log(`Got authentication ticket: ${authTicket}`);
+      this.authTicket = authTicket;
+      this.updateDom();
+
+      // Schedule the first call to the card accounts API.
+      setTimeout(() => {
+        this.getCardAccounts();
+      }, this.config.updateInterval);
+    } else if (notification === "CARD_ACCOUNTS_RESULT") {
+      if (payload.error) {
+        console.error(`Error getting card accounts: ${payload.error}`);
+        setTimeout(() => {
+          this.getCardAccounts();
+        }, this.config.retryDelay);
+        return;
+      }
+
+      const cardAccounts = payload.cardAccounts;
+      if (!cardAccounts) {
+        console.error("Error: Unable to retrieve card accounts.");
+        setTimeout(() => {
+          this.getCardAccounts();
+        }, this.config.retryDelay);
+        return;
+      }
+
+      console.log(`Got card accounts: ${JSON.stringify(cardAccounts)}`);
+      this.cardAccounts = cardAccounts;
+      this.updateDom();
+
+      // Schedule the next call to the card accounts API.
+      setTimeout(() => {
+        this.getCardAccounts();
+      }, this.config.updateInterval);
+    } else {
+      console.warn(`Unknown socket notification received: ${notification}`);
+    }
+  },
+
+  getCardAccounts: function() {
+    console.log("Retrieving card accounts");
+
+    const options = {
+      method: "GET",
+      url: `${this.config.apiUrl}/user/cardaccounts`,
+      headers: {
+        "AuthenticationTicket": this.authTicket
+      }
+    };
+
+    this.sendSocketNotification("GET_CARD_ACCOUNTS", options);
+  }
+});
