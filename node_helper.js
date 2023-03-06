c263be928261c7be93d41dc16a8b0c56db5fa143
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-03-07 00:01:43 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -11,176 +11,90 @@ module.exports = NodeHelper.create({
 
     if (notification === "GET_AUTH_TICKET") {
       this.config = payload;
-      this.getAuthTicket();
-    } else if (notification === "GET_CARD_ACCOUNTS") {
+      console.log("Retrieving authentication ticket");
+
+      const authHeader = `Basic ${Buffer.from(`${payload.username}:${payload.password}`).toString("base64")}`;
+      const options = {
+        method: "GET",
+        url: `${payload.apiUrl}/login`,
+        headers: {
+          "Authorization": authHeader
+        }
+      };
+
+      this.makeRequest(options);
+    } else if (notification === "GET_CARD_ACCOUNTS" || notification === "GET_MIN_BONUS" || notification === "GET_STORES" || notification === "GET_OFFERS") {
       const options = payload;
-      options.headers["AuthenticationTicket"] = this.authTicket;
-      this.makeRequest(options, "CARD_ACCOUNTS_RESULT");
-    } else if (notification === "GET_STORES") {
-      const options = {
-        method: "GET",
-        url: `${this.config.storeApiUrl}/user/stores`,
-        headers: {
-          "AuthenticationTicket": this.authTicket
-        }
-      };
-      this.makeRequest(options, "STORES_RESULT");
-    } else if (notification === "GET_MINBONUS_TRANSACTIONS") {
-      const options = {
-        method: "GET",
-        url: `${this.config.apiUrl}/user/minbonustransaction`,
-        headers: {
-          "AuthenticationTicket": this.authTicket
-        }
-      };
-      this.makeRequest(options, "MINBONUS_TRANSACTIONS_RESULT");
-    } else if (notification === "GET_OFFERS") {
-      this.getOffers();
+      this.makeRequest(options, notification);
     }
   },
 
-  makeRequest: function(options, resultNotification) {
-    const self = this;
+  makeRequest: function(options, notification = "") {
+    var self = this;
     request(options, function(error, response, body) {
       if (!error && response.statusCode === 200) {
-        const result = JSON.parse(body);
-        console.log(`Got ${resultNotification}:`, result);
-        self.sendSocketNotification(resultNotification, { result: result });
+        if (notification === "GET_AUTH_TICKET") {
+          const authTicket = response.headers["authenticationticket"];
+          console.log(response.headers); // Add this line
+          if (!authTicket) {
+            console.error("Error: Unable to retrieve authentication ticket.");
+            self.sendSocketNotification("AUTH_TICKET_RESULT", { error: "Unable to retrieve authentication ticket." });
+            return;
+          }
+
+          console.log(`Got authentication ticket: ${authTicket}`);
+          self.authTicket = authTicket;
+
+          const cardAccountsOptions = {
+            method: "GET",
+            url: `${self.config.apiUrl}/user/cardaccounts`,
+            headers: {
+              "AuthenticationTicket": authTicket
+            }
+          };
+
+          self.makeRequest(cardAccountsOptions, "CARD_ACCOUNTS_RESULT");
+
+          if (self.config.settings.apiEndpoints.minbonus) {
+            const minBonusOptions = {
+              method: "GET",
+              url: `${self.config.apiUrl}/user/minbonustransaction`,
+              headers: {
+                "AuthenticationTicket": authTicket
+              }
+            };
+            self.makeRequest(minBonusOptions, "MIN_BONUS_RESULT");
+          }
+
+          if (self.config.settings.apiEndpoints.stores) {
+            const storesOptions = {
+              method: "GET",
+              url: `${self.config.storeApiUrl}/user/stores`,
+              headers: {
+                "AuthenticationTicket": authTicket
+              }
+            };
+            self.makeRequest(storesOptions, "STORES_RESULT");
+          }
+
+          if (self.config.settings.apiEndpoints.showoffer && self.config.settings.apiEndpoints.offerstoreid) {
+            const offersOptions = {
+              method: "GET",
+              url: `${self.config.apiUrl}/offers?Stores=${self.config.settings.apiEndpoints.offerstoreid}`,
+              headers: {
+                "AuthenticationTicket": authTicket
+              }
+            };
+            self.makeRequest(offersOptions, "OFFERS_RESULT");
+          }
+        } else {
+          const result = JSON.parse(body);
+          self.sendSocketNotification(`${notification}`, { [`${notification.toLowerCase().replace("get_", "")}`]: result });
+        }
       } else {
-        console.error(`Error getting ${resultNotification}: ${error}`);
-        self.sendSocketNotification(resultNotification, { error: error });
+        console.error(`Error getting ${notification.toLowerCase().replace("get_", "")}: ${error}`);
+        self.sendSocketNotification(`${notification}_RESULT`, { error: error });
       }
     });
-  },
-
-  getAuthTicket: function() {
-    console.log("Retrieving authentication ticket");
-
-    const authHeader = `Basic ${Buffer.from(`${this.config.username}:${this.config.password}`).toString("base64")}`;
-    const options = {
-      method: "GET",
-      url: `${this.config.apiUrl}/login`,
-      headers: {
-        "Authorization": authHeader
-      }
-    };
-
-    const self = this;
-    request(options, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-        const result = JSON.parse(body);
-        const authTicket = result.AuthenticationTicket;
-        console.log(`Got authentication ticket: ${authTicket}`);
-        self.authTicket = authTicket;
-        self.startRequests();
-      } else {
-        console.error(`Error getting authentication ticket: ${error}`);
-        self.authTicket = "";
-        setTimeout(() => {
-          self.getAuthTicket();
-        }, self.config.retryDelay);
-      }
-    });
-  },
-
-  startRequests: function() {
-    // Schedule the first call to the card accounts API.
-    setTimeout(() => {
-      this.getCardAccounts();
-    }, this.config.updateInterval);
-
-    // Schedule the first call to the stores API.
-    setTimeout(() => {
-      this.getStores();
-    }, this.config.updateInterval);
-
-    // Schedule the first call to the minbonus transactions API.
-    setTimeout(() => {
-      this.getMinBonusTransactions();
-    }, this.config.updateInterval);
-
-    // Schedule the first call to the offers API.
-    setTimeout(() => {
-      this.getOffers();
-    }, this.config.updateInterval);
-  },
-  },
-
-startRequests: function() {
-// Schedule the first call to the card accounts API.
-setTimeout(() => {
-this.getCardAccounts();
-}, this.config.updateInterval);
-  // Schedule the first call to the stores API.
-setTimeout(() => {
-  this.getStores();
-}, this.config.updateInterval);
-
-// Schedule the first call to the minbonus transactions API.
-setTimeout(() => {
-  this.getMinBonusTransactions();
-}, this.config.updateInterval);
-
-// Schedule the first call to the offers API.
-setTimeout(() => {
-  this.getOffers();
-}, this.config.updateInterval);
-},
-
-getCardAccounts: function() {
-console.log("Retrieving card accounts");
-  const options = {
-  method: "GET",
-  url: `${this.config.apiUrl}/user/cardaccounts`,
-  headers: {
-    "AuthenticationTicket": this.authTicket
   }
-};
-
-this.sendSocketNotification("GET_CARD_ACCOUNTS", options);
-},
-
-getStores: function() {
-console.log("Retrieving stores");
-  const options = {
-  method: "GET",
-  url: `${this.config.storeApiUrl}/user/stores`,
-  headers: {
-    "AuthenticationTicket": this.authTicket
-  }
-};
-
-this.sendSocketNotification("GET_STORES", options);
-},
-
-getMinBonus: function() {
-console.log("Retrieving min bonus");
-  const options = {
-  method: "GET",
-  url: `${this.config.apiUrl}/user/minbonustransaction`,
-  headers: {
-    "AuthenticationTicket": this.authTicket
-  }
-};
-
-this.sendSocketNotification("GET_MIN_BONUS", options);
-},
-
-getOffers: function() {
-console.log("Retrieving offers");
-  let url = `${this.config.apiUrl}/offers`;
-if (this.config.settings.apiEndpoints.offers.storeId) {
-  url += `?Stores=${this.config.settings.apiEndpoints.offers.storeId}`;
-}
-
-const options = {
-  method: "GET",
-  url: url,
-  headers: {
-    "AuthenticationTicket": this.authTicket
-  }
-};
-
-this.sendSocketNotification("GET_OFFERS", options);
-}
 });
