14dcecb8c6c8c63e78ebb285682536cafb9b8438
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-03-07 14:12:29 +0100

    Update MMM-ICA.js

--- MMM-ICA.js
+++ MMM-ICA.js
@@ -21,12 +21,14 @@ Module.register("MMM-ICA", {
     this.sendSocketNotification("GET_AUTH_TICKET", this.config);
   },
 
-getDom: function() {
+  getDom: function() {
     const wrapper = document.createElement("div");
     wrapper.className = "small bright";
 
-    if (this.cardAccounts) {
-      wrapper.innerHTML = `Saldo: ${this.cardAccounts.Cards[0].Accounts[0].Balance}`;
+    if (this.cardAccounts && this.minBonus) {
+      const balance = this.cardAccounts.Cards[0].Accounts[0].Balance;
+      const bonus = this.minBonus.MinBonus;
+      wrapper.innerHTML = `Saldo: ${balance} | Bonus: ${bonus}`;
     } else {
       wrapper.innerHTML = "Loading content...";
     }
@@ -67,6 +69,7 @@ getDom: function() {
       // Schedule the first call to the card accounts API.
       setTimeout(() => {
         this.getCardAccounts();
+        this.getMinBonus(); // Call getMinBonus here
       }, this.config.updateInterval);
     } else if (notification === "CARD_ACCOUNTS_RESULT") {
       if (payload.error) {
@@ -94,6 +97,31 @@ getDom: function() {
       setTimeout(() => {
         this.getCardAccounts();
       }, this.config.updateInterval);
+    } else if (notification === "MIN_BONUS_RESULT") {
+      if (payload.error) {
+        console.error(`Error getting ICA bonus: ${payload.error}`);
+        setTimeout(() => {
+          this.getMinBonus();
+        }, this.config.retryDelay);
+        return;
+      }
+      const minBonus = payload.minBonus;
+      if (!minBonus)
+        console.error("Error: Unable to retrieve ICA bonus.");
+        setTimeout(() => {
+          this.getMinBonus();
+        }, this.config.retryDelay);
+        return;
+      }
+
+      console.log(`Got ICA bonus: ${JSON.stringify(minBonus)}`);
+      this.minBonus = minBonus;
+      this.updateDom();
+
+      // Schedule the next call to the ICA bonus API.
+      setTimeout(() => {
+        this.getMinBonus();
+      }, this.config.updateInterval);
     } else {
       console.warn(`Unknown socket notification received: ${notification}`);
     }
@@ -111,5 +139,19 @@ getDom: function() {
     };
 
     this.sendSocketNotification("GET_CARD_ACCOUNTS", options);
+  },
+
+  getMinBonus: function() {
+    console.log("Retrieving ICA bonus");
+
+    const options = {
+      method: "GET",
+      url: `${this.config.apiUrl}/user/minbonustransaction`,
+      headers: {
+        "AuthenticationTicket": this.authTicket
+      }
+    };
+
+    this.sendSocketNotification("GET_MIN_BONUS", options);
   }
 });
