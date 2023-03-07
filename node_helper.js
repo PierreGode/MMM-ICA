6747330668ada227876303da508a71adac0afbff
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-03-07 10:06:12 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,4 +1,3 @@
-// node_helper.js
 const NodeHelper = require("node_helper");
 const request = require("request");
 
@@ -24,30 +23,48 @@ module.exports = NodeHelper.create({
       };
 
       this.makeRequest(options);
-    } else if (notification === "GET_STORES") {
-      const options = payload;
-      console.log("Retrieving stores");
-
-      this.makeRequest(options, "STORES_RESULT");
     } else if (notification === "GET_CARD_ACCOUNTS") {
       const options = payload;
-      console.log("Retrieving card accounts");
-
-      this.makeRequest(options, "CARD_ACCOUNTS_RESULT");
+      this.makeRequest(options, this.handleCardAccountsResult);
+    } else if (notification === "GET_STORES") {
+      const options = payload;
+      this.makeRequest(options, this.handleStoresResult);
+    } else {
+      console.warn(`Unknown socket notification received: ${notification}`);
     }
   },
 
-  makeRequest: function(options, resultNotification) {
+  makeRequest: function(options, callback) {
     var self = this;
     request(options, function(error, response, body) {
       if (!error && response.statusCode === 200) {
-        const result = JSON.parse(body);
-        console.log(`Got result: ${JSON.stringify(result)}`);
-        self.sendSocketNotification(resultNotification, { [resultNotification.toLowerCase().replace("_result", "")]: result });
+        callback.call(self, body);
       } else {
-        console.error(`Error getting data: ${error}`);
-        self.sendSocketNotification(resultNotification, { error: error });
+        console.error(`Error: ${error}`);
+        self.sendSocketNotification(options.callbackNotification, { error: error });
       }
     });
+  },
+  
+  handleCardAccountsResult: function(body) {
+    try {
+      const cardAccounts = JSON.parse(body);
+      console.log("Got card accounts:", cardAccounts);
+      this.sendSocketNotification("CARD_ACCOUNTS_RESULT", { cardAccounts: cardAccounts });
+    } catch (error) {
+      console.error(`Error parsing card accounts JSON: ${error}`);
+      this.sendSocketNotification("CARD_ACCOUNTS_RESULT", { error: error });
+    }
+  },
+
+  handleStoresResult: function(body) {
+    try {
+      const stores = JSON.parse(body);
+      console.log("Got stores:", stores);
+      this.sendSocketNotification("STORES_RESULT", { stores: stores });
+    } catch (error) {
+      console.error(`Error parsing stores JSON: ${error}`);
+      this.sendSocketNotification("STORES_RESULT", { error: error });
+    }
   }
 });
