a83b464fd20f838e5d74dde5413470ba2db0654e
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-03-07 12:10:55 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,14 +1,15 @@
-const NodeHelper = require("node_helper");
-const request = require("request");
+const request = require('request');
+const NodeHelper = require('node_helper');
 
 module.exports = NodeHelper.create({
   start: function() {
     console.log(`Starting helper: ${this.name}`);
+    this.authTickets = {};
+    this.endpointData = {};
   },
 
   socketNotificationReceived: function(notification, payload) {
     console.log("Received socket notification:", notification, "with payload:", payload);
-
     if (notification === "GET_AUTH_TICKET") {
       this.config = payload;
       console.log("Retrieving authentication ticket");
@@ -31,13 +32,12 @@ module.exports = NodeHelper.create({
             this.sendSocketNotification("AUTH_TICKET_RESULT", { error: "Unable to retrieve authentication ticket." });
             return;
           }
-
           console.log(`Got authentication ticket: ${authTicket}`);
           this.authTickets[payload.index] = authTicket;
 
           const cardAccountsOptions = {
             method: "GET",
-            url: `${payload.apiUrl}/user/cardaccounts`,
+            url: `${payload.config.apiUrl}/user/cardaccounts`,
             headers: {
               "AuthenticationTicket": authTicket
             }
@@ -60,7 +60,6 @@ module.exports = NodeHelper.create({
           const cardAccounts = JSON.parse(body);
           console.log(`Got card accounts for endpoint ${index}:`, cardAccounts);
           this.endpointData[index].cardAccounts = cardAccounts;
-
           this.sendSocketNotification("CARD_ACCOUNTS_RESULT", { index: index, cardAccounts: cardAccounts });
         } else {
           console.error(`Error getting card accounts for endpoint ${index}: ${error}`);
@@ -73,43 +72,4 @@ module.exports = NodeHelper.create({
   makeRequest: function(options, callback) {
     request(options, callback);
   },
-    createTransaction: function(cardAccountIndex, transactionData) {
-    const endpointIndex = this.config.cardAccounts[cardAccountIndex].endpointIndex;
-    const cardAccountId = this.endpointData[endpointIndex].cardAccounts[cardAccountIndex].id;
 
-    const transactionOptions = {
-      method: "POST",
-      url: `${this.config.apiUrl}/user/cardaccounts/${cardAccountId}/transactions`,
-      headers: {
-        "AuthenticationTicket": this.authTickets[endpointIndex],
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(transactionData)
-    };
-
-    console.log(`Creating transaction for card account ${cardAccountId}:`, transactionData);
-    this.makeRequest(transactionOptions, (error, response, body) => {
-      if (!error && response.statusCode === 201) {
-        console.log(`Transaction created for card account ${cardAccountId}.`);
-        this.sendSocketNotification("TRANSACTION_CREATED", { cardAccountIndex: cardAccountIndex, transaction: transactionData });
-      } else {
-        console.error(`Error creating transaction for card account ${cardAccountId}: ${error}`);
-        this.sendSocketNotification("TRANSACTION_CREATED", { cardAccountIndex: cardAccountIndex, error: error });
-      }
-    });
-  },
-
-  getEndpointData: function() {
-    const endpointData = [];
-    const endpointCount = this.config.endpoints.length;
-
-    for (let i = 0; i < endpointCount; i++) {
-      endpointData.push({
-        endpoint: this.config.endpoints[i],
-        cardAccounts: []
-      });
-    }
-
-    return endpointData;
-  }
-});
