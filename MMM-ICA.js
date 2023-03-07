d6091bdd203bc0d70c3ee64f828e343a399213e3
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-03-07 12:16:04 +0100

    Update MMM-ICA.js

--- MMM-ICA.js
+++ MMM-ICA.js
@@ -1,6 +1,8 @@
 Module.register("MMM-ICA", {
   defaults: {
-    endpoints: [], // Array of endpoint configurations.
+    username: "",
+    password: "",
+    apiUrl: "",
     updateInterval: 60 * 60 * 1000, // Update every hour.
     retryDelay: 5 * 60 * 1000 // Retry every 5 minutes if an error occurs.
   },
@@ -9,48 +11,25 @@ Module.register("MMM-ICA", {
     console.log("Module config:", this.config);
     Log.info(`Starting module: ${this.name}`);
 
-    if (!this.config.endpoints || this.config.endpoints.length === 0) {
-      console.error("Error: endpoints not provided in module config.");
+    if (!this.config.username || !this.config.password) {
+      console.error("Error: username or password not provided in module config.");
+      this.authTicket = "";
+      this.updateDom();
       return;
     }
 
-    // Initialize the endpoint data.
-    this.endpointData = {};
-
-    // Initialize the authentication tickets.
-    this.authTickets = {};
-
-    // Start the update cycle for each endpoint.
-    this.config.endpoints.forEach((endpoint, index) => {
-      // Set the initial state for the endpoint data.
-      this.endpointData[index] = {};
-
-      // Get the authentication ticket for the endpoint.
-      if (!endpoint.username || !endpoint.password || !endpoint.apiUrl) {
-        console.error(`Error: Invalid configuration for endpoint ${index}.`);
-        return;
-      }
-      this.sendSocketNotification("GET_AUTH_TICKET", {index: index, config: endpoint});
-    });
+    this.sendSocketNotification("GET_AUTH_TICKET", this.config);
   },
 
-  getDom: function() {
+getDom: function() {
     const wrapper = document.createElement("div");
     wrapper.className = "small bright";
 
-    // Iterate over each endpoint to display the data.
-    this.config.endpoints.forEach((endpoint, index) => {
-      const endpointData = this.endpointData[index];
-      const endpointWrapper = document.createElement("div");
-
-      if (endpointData.cardAccounts) {
-        endpointWrapper.innerHTML = `Endpoint ${index}: Saldo: ${endpointData.cardAccounts.Cards[0].Accounts[0].Balance}`;
-      } else {
-        endpointWrapper.innerHTML = `Endpoint ${index}: Loading content...`;
-      }
-
-      wrapper.appendChild(endpointWrapper);
-    });
+    if (this.cardAccounts) {
+      wrapper.innerHTML = `Saldo: ${this.cardAccounts.Cards[0].Accounts[0].Balance}`;
+    } else {
+      wrapper.innerHTML = "Loading content...";
+    }
 
     return wrapper;
   },
@@ -60,81 +39,89 @@ Module.register("MMM-ICA", {
     console.log("Received socket notification:", notification, "with payload:", payload);
 
     if (notification === "AUTH_TICKET_RESULT") {
-      const index = payload.index;
-      const config = this.config.endpoints[index];
-
       if (payload.error) {
-        console.error(`Error getting authentication ticket for endpoint ${index}: ${payload.error}`);
-        this.authTickets[index] = "";
+        console.error(`Error getting authentication ticket: ${payload.error}`);
+        this.authTicket = "";
+        this.updateDom();
         setTimeout(() => {
-          this.sendSocketNotification("GET_AUTH_TICKET", {index: index, config: config});
+          this.sendSocketNotification("GET_AUTH_TICKET", this.config);
         }, this.config.retryDelay);
         return;
       }
 
       const authTicket = payload.authTicket;
       if (!authTicket) {
-        console.error(`Error: Unable to retrieve authentication ticket for endpoint ${index}.`);
-        this.authTickets[index] = "";
+        console.error("Error: Unable to retrieve authentication ticket.");
+        this.authTicket = "";
+        this.updateDom();
         setTimeout(() => {
-          this.sendSocketNotification("GET_AUTH_TICKET", {index: index, config: config});
+          this.sendSocketNotification("GET_AUTH_TICKET", this.config);
         }, this.config.retryDelay);
         return;
       }
 
-      console.log(`Got authentication ticket for endpoint ${index}: ${authTicket}`);
-      this.authTickets[index] = authTicket;
+      console.log(`Got authentication ticket: ${authTicket}`);
+      this.authTicket = authTicket;
+      this.updateDom();
 
       // Schedule the first call to the card accounts API.
       setTimeout(() => {
-        this.getCardAccounts(index);
+        this.getCardAccounts();
       }, this.config.updateInterval);
     } else if (notification === "CARD_ACCOUNTS_RESULT") {
-      const index = payload.index;
-      const config = this.config.endpoints[index];
-
       if (payload.error) {
-        console.error(`Error getting card accounts for endpoint ${index}: ${payload.error}`);
+        console.error(`Error getting card accounts: ${payload.error}`);
         setTimeout(() => {
-          this.getCardAccounts(index);
+          this.getCardAccounts();
         }, this.config.retryDelay);
         return;
       }
+
       const cardAccounts = payload.cardAccounts;
       if (!cardAccounts) {
-        console.error(`Error: Unable to retrieve card accounts for endpoint ${index}.`);
+        console.error("Error: Unable to retrieve card accounts.");
         setTimeout(() => {
-          this.getCardAccounts(index);
+          this.getCardAccounts();
         }, this.config.retryDelay);
         return;
       }
 
-      console.log(`Got card accounts for endpoint ${index}: ${JSON.stringify(cardAccounts)}`);
-      this.endpointData[index].cardAccounts = cardAccounts;
+      console.log(`Got card accounts: ${JSON.stringify(cardAccounts)}`);
+      this.cardAccounts = cardAccounts;
+      this.updateDom();
 
       // Schedule the next call to the card accounts API.
       setTimeout(() => {
-        this.getCardAccounts(index);
+        this.getCardAccounts();
       }, this.config.updateInterval);
     } else {
       console.warn(`Unknown socket notification received: ${notification}`);
     }
-  },
+  }
+  getStores: function() {
+  console.log("Retrieving stores");
 
-  getCardAccounts: function(index) {
-    console.log(`Retrieving card accounts for endpoint ${index}`);
+  const options = {
+    method: "GET",
+    url: `${this.config.apiUrl}/user/stores`,
+    headers: {
+      "AuthenticationTicket": this.authTicket
+    }
+  };
 
-    const endpoint = this.config.endpoints[index];
-    const authTicket = this.authTickets[index];
+  this.sendSocketNotification("GET_STORES", options);
+}
+  getCardAccounts: function() {
+    console.log("Retrieving card accounts");
 
     const options = {
       method: "GET",
-      url: `${endpoint.apiUrl}/user/cardaccounts`,
+      url: `${this.config.apiUrl}/user/cardaccounts`,
       headers: {
-        "AuthenticationTicket": authTicket
+        "AuthenticationTicket": this.authTicket
       }
     };
 
-    this.sendSocketNotification("GET_CARD_ACCOUNTS", {index: index, options: options});
+    this.sendSocketNotification("GET_CARD_ACCOUNTS", options);
   }
 });
