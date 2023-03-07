671e5ab36f0ca545d3fb8f3cc7801eefb404a960
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-03-07 09:13:17 +0100

    Update MMM-ICA.js

--- MMM-ICA.js
+++ MMM-ICA.js
@@ -3,14 +3,8 @@ Module.register("MMM-ICA", {
     username: "",
     password: "",
     apiUrl: "",
-    storeApiUrl: "",
-    updateInterval: 60 * 60 * 1000,
-    retryDelay: 10 * 60 * 1000,
-    apiEndpoints: {
-      stores: true,
-      minbonus: true,
-      offers: ""
-    }
+    updateInterval: 60 * 60 * 1000, // Update every hour.
+    retryDelay: 5 * 60 * 1000 // Retry every 5 minutes if an error occurs.
   },
 
   start: function() {
@@ -27,33 +21,20 @@ Module.register("MMM-ICA", {
     this.sendSocketNotification("GET_AUTH_TICKET", this.config);
   },
 
-  getDom: function() {
+getDom: function() {
     const wrapper = document.createElement("div");
     wrapper.className = "small bright";
 
     if (this.cardAccounts) {
-      const apiEndpoints = this.config.apiEndpoints;
-
-      if (apiEndpoints.minbonus) {
-        wrapper.innerHTML = `Min bonus: ${this.cardAccounts.minBonus}`;
-      } else if (apiEndpoints.stores) {
-        const stores = this.cardAccounts.stores;
-        const storeList = stores.map((store) => `${store.StoreId} - ${store.Name}`).join("<br>");
-        wrapper.innerHTML = `Stores:<br>${storeList}`;
-      } else if (apiEndpoints.offers) {
-        const offers = this.cardAccounts.offers.Offers;
-        const offerList = offers.map((offer) => `${offer.Title} (${offer.Price} kr)`).join("<br>");
-        wrapper.innerHTML = `Offers:<br>${offerList}`;
-      } else {
-        wrapper.innerHTML = "Invalid API endpoint configuration.";
-      }
+      wrapper.innerHTML = `Saldo: ${this.cardAccounts.Cards[0].Accounts[0].Balance}`;
     } else {
-      wrapper.innerHTML = "Loading content...";
+      wrapper.innerHTML = "Waiting for card accounts...";
     }
 
     return wrapper;
   },
 
+  // Override socket notification handler.
   socketNotificationReceived: function(notification, payload) {
     console.log("Received socket notification:", notification, "with payload:", payload);
 
@@ -83,6 +64,7 @@ Module.register("MMM-ICA", {
       this.authTicket = authTicket;
       this.updateDom();
 
+      // Schedule the first call to the card accounts API.
       setTimeout(() => {
         this.getCardAccounts();
       }, this.config.updateInterval);
@@ -95,29 +77,20 @@ Module.register("MMM-ICA", {
         return;
       }
 
-      const apiEndpoints = this.config.apiEndpoints;
-      let cardAccounts = {};
-
-      if (apiEndpoints.minbonus) {
-        cardAccounts.minBonus = payload.minBonus;
-      }
-
-      if (apiEndpoints.stores) {
-        cardAccounts.stores = payload.stores;
-      }
-
-      if (apiEndpoints.offers) {
-        const storeId = apiEndpoints.offers;
-        const offers = payload.offers.filter((offer) => offer;
-               .find((store) => store.StoreId === storeId)
-          .Offers;
-        cardAccounts.offers = { Offers: offers };
+      const cardAccounts = payload.cardAccounts;
+      if (!cardAccounts) {
+        console.error("Error: Unable to retrieve card accounts.");
+        setTimeout(() => {
+          this.getCardAccounts();
+        }, this.config.retryDelay);
+        return;
       }
 
       console.log(`Got card accounts: ${JSON.stringify(cardAccounts)}`);
       this.cardAccounts = cardAccounts;
       this.updateDom();
 
+      // Schedule the next call to the card accounts API.
       setTimeout(() => {
         this.getCardAccounts();
       }, this.config.updateInterval);
@@ -129,28 +102,14 @@ Module.register("MMM-ICA", {
   getCardAccounts: function() {
     console.log("Retrieving card accounts");
 
-    const apiEndpoints = this.config.apiEndpoints;
     const options = {
       method: "GET",
-      url: "",
+      url: `${this.config.apiUrl}/user/cardaccounts`,
       headers: {
         "AuthenticationTicket": this.authTicket
       }
     };
 
-    if (apiEndpoints.minbonus) {
-      options.url = `${this.config.apiUrl}/user/minbonustransaction`;
-    } else if (apiEndpoints.stores) {
-      options.url = `${this.config.apiUrl}/stores`;
-    } else if (apiEndpoints.offers) {
-      options.url = `${this.config.storeApiUrl}/offers`;
-    } else {
-      console.error("Error: Invalid API endpoint configuration.");
-      return;
-    }
-
     this.sendSocketNotification("GET_CARD_ACCOUNTS", options);
   }
 });
-
-        
