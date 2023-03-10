6a650c72e61cf7c31fcac853855019006e0dd488
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-03-10 12:05:45 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -47,17 +47,16 @@ module.exports = NodeHelper.create({
         console.log(`Got authentication ticket: ${authTicket}`);
         self.authTicket = authTicket;
 
-        const cardAccountsOptions = {
-          method: "GET",
-          url: `${self.config.apiUrl}/user/cardaccounts`,
-          headers: {
-            "AuthenticationTicket": authTicket
-          }
-        };
-
         if (self.config.settings.StoreID && self.config.settings.DisplayStoreID) {
           self.getStore();
         } else {
+          const cardAccountsOptions = {
+            method: "GET",
+            url: `${self.config.apiUrl}/user/cardaccounts`,
+            headers: {
+              "AuthenticationTicket": authTicket
+            }
+          };
           self.makeCardAccountsRequest(cardAccountsOptions);
         }
       } else {
@@ -91,50 +90,14 @@ module.exports = NodeHelper.create({
       }
     });
   },
-  makeFavoriteStoresRequest: function(options) {
+
+makeFavoriteStoresRequest: function(options) {
     var self = this;
     request(options, function(error, response, body) {
       if (!error && response.statusCode === 200) {
         const favoriteStores = JSON.parse(body);
         console.log("Got favorite stores:", favoriteStores);
         self.sendSocketNotification("FAVORITE_STORES_RESULT", { favoriteStores: favoriteStores });
-
-        if (self.config.settings.StoreID) {
-          self.getStore();
-        } else {
-          const cardAccountsOptions = {
-            method: "GET",
-            url: `${self.config.apiUrl}/user/cardaccounts`,
-            headers: {
-              "AuthenticationTicket": self.authTicket
-            }
-          };
-          self.makeCardAccountsRequest(cardAccountsOptions);
-        }
-      } else {
-        console.error(`Error getting favorite stores: ${error}`);
-        self.sendSocketNotification("FAVORITE_STORES_RESULT", { error: error });
-      }
-    });
-  },
-
-  getStore: function() {
-    var self = this;
-    const storeId = self.config.settings.StoreID;
-    console.log("Retrieving store", storeId);
-    const options = {
-      method: "GET",
-      url: `${self.config.apiUrl}/stores/${storeId}`,
-      headers: {
-        "AuthenticationTicket": self.authTicket
-      }
-    };
-
-    request(options, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-        const store = JSON.parse(body);
-        console.log("Got store:", store);
-        self.sendSocketNotification("STORE_RESULT", { store: store });
         const cardAccountsOptions = {
           method: "GET",
           url: `${self.config.apiUrl}/user/cardaccounts`,
@@ -144,9 +107,36 @@ module.exports = NodeHelper.create({
         };
         self.makeCardAccountsRequest(cardAccountsOptions);
       } else {
-        console.error(`Error getting store ${storeId}: ${error}`);
-        self.sendSocketNotification("STORE_RESULT", { error: error });
+        console.error(`Error getting favorite stores: ${error}`);
+        self.sendSocketNotification("FAVORITE_STORES_RESULT", { error: error });
+      }
+    });
+  },
+
+  getStore: function() {
+    var self = this;
+    const options = {
+      method: "GET",
+      url: `${self.config.storeApiUrl}/stores/${self.config.settings.StoreID}`,
+      headers: {
+        "AuthenticationTicket": self.authTicket
+      }
+    };
+    self.makeStoreRequest(options);
+  },
+
+  makeStoreRequest: function(options) {
+    var self = this;
+    request(options, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        const store = JSON.parse(body);
+        console.log("Got store:", store);
+        self.sendSocketNotification("GET_STORE_RESULT", { store: store });
+      } else {
+        console.error(`Error getting store: ${error}`);
+        self.sendSocketNotification("GET_STORE_RESULT", { error: error });
       }
     });
   }
 });
+
