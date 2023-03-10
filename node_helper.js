5cc194b455cb35ee1681dda59ea5ef408c7616a3
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-03-10 12:08:06 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -91,21 +91,13 @@ module.exports = NodeHelper.create({
     });
   },
 
-makeFavoriteStoresRequest: function(options) {
+  makeFavoriteStoresRequest: function(options) {
     var self = this;
     request(options, function(error, response, body) {
       if (!error && response.statusCode === 200) {
         const favoriteStores = JSON.parse(body);
         console.log("Got favorite stores:", favoriteStores);
         self.sendSocketNotification("FAVORITE_STORES_RESULT", { favoriteStores: favoriteStores });
-        const cardAccountsOptions = {
-          method: "GET",
-          url: `${self.config.apiUrl}/user/cardaccounts`,
-          headers: {
-            "AuthenticationTicket": self.authTicket
-          }
-        };
-        self.makeCardAccountsRequest(cardAccountsOptions);
       } else {
         console.error(`Error getting favorite stores: ${error}`);
         self.sendSocketNotification("FAVORITE_STORES_RESULT", { error: error });
