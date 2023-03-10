650b979b51e1fddf98a5eeb8fd3b84bcb37625a0
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-03-10 12:12:09 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -91,62 +91,61 @@ module.exports = NodeHelper.create({
       }
     });
   },
-  makeFavoriteStoresRequest: function(options) {
-    var self = this;
-    request(options, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-        const favoriteStores = JSON.parse(body);
-        console.log("Got favorite stores:", favoriteStores);
-        self.sendSocketNotification("FAVORITE_STORES_RESULT", { favoriteStores: favoriteStores });
 
-        if (self.config.settings.StoreID) {
-          self.getStore();
-        } else {
-          const cardAccountsOptions = {
-            method: "GET",
-            url: `${self.config.apiUrl}/user/cardaccounts`,
-            headers: {
-              "AuthenticationTicket": self.authTicket
-            }
-          };
-          self.makeCardAccountsRequest(cardAccountsOptions);
+makeFavoriteStoresRequest: function(options) {
+var self = this;
+request(options, function(error, response, body) {
+if (!error && response.statusCode === 200) {
+const favoriteStores = JSON.parse(body);
+console.log("Got favorite stores:", favoriteStores);
+self.sendSocketNotification("FAVORITE_STORES_RESULT", { favoriteStores: favoriteStores });
+      if (self.config.settings.StoreID) {
+      self.getStore();
+    } else {
+      const cardAccountsOptions = {
+        method: "GET",
+        url: `${self.config.apiUrl}/user/cardaccounts`,
+        headers: {
+          "AuthenticationTicket": self.authTicket
         }
-      } else {
-        console.error(`Error getting favorite stores: ${error}`);
-        self.sendSocketNotification("FAVORITE_STORES_RESULT", { error: error });
-      }
-    });
-  },
+      };
+      self.makeCardAccountsRequest(cardAccountsOptions);
+    }
+  } else {
+    console.error(`Error getting favorite stores: ${error}`);
+    self.sendSocketNotification("FAVORITE_STORES_RESULT", { error: error });
+  }
+});
+},
 
-  getStore: function() {
-    var self = this;
-    const storeId = self.config.settings.StoreID;
-    console.log("Retrieving store", storeId);
-    const options = {
+getStore: function() {
+var self = this;
+const storeId = self.config.settings.StoreID;
+console.log("Retrieving store", storeId);
+const options = {
+method: "GET",
+url: ${self.config.apiUrl}/stores/${storeId},
+headers: {
+"AuthenticationTicket": self.authTicket
+}
+};
+  request(options, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const store = JSON.parse(body);
+    console.log("Got store:", store);
+    self.sendSocketNotification("STORE_RESULT", { store: store });
+    const cardAccountsOptions = {
       method: "GET",
-      url: `${self.config.apiUrl}/stores/${storeId}`,
+      url: `${self.config.apiUrl}/user/cardaccounts`,
       headers: {
         "AuthenticationTicket": self.authTicket
       }
     };
-
-    request(options, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-        const store = JSON.parse(body);
-        console.log("Got store:", store);
-        self.sendSocketNotification("STORE_RESULT", { store: store });
-        const cardAccountsOptions = {
-          method: "GET",
-          url: `${self.config.apiUrl}/user/cardaccounts`,
-          headers: {
-            "AuthenticationTicket": self.authTicket
-          }
-        };
-        self.makeCardAccountsRequest(cardAccountsOptions);
-      } else {
-        console.error(`Error getting store ${storeId}: ${error}`);
-        self.sendSocketNotification("STORE_RESULT", { error: error });
-      }
-    });
+    self.makeCardAccountsRequest(cardAccountsOptions);
+  } else {
+    console.error(`Error getting store ${storeId}: ${error}`);
+    self.sendSocketNotification("STORE_RESULT", { error: error });
   }
+});
+}
 });
