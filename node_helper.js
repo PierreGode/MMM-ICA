91067da20e9844c40798e45ad83ee6671d6adaac
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-03-13 08:14:57 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -23,50 +23,62 @@ module.exports = NodeHelper.create({
       };
 
       this.makeRequest(options);
-      // Schedule a refresh of the authentication ticket every 60 minutes
-      setInterval(() => {
-        console.log("Refreshing authentication ticket");
-        this.makeRequest(options);
-      }, 60 * 60 * 1000); // 60 minutes * 60 seconds * 1000 milliseconds
     }
   },
 
-makeRequest: function(options) {
-  var self = this;
-  request(options, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      const authTicket = response.headers["authenticationticket"];
-      console.log(response.headers); // Add this line
-      if (!authTicket) {
-        console.error("Error: Unable to retrieve authentication ticket.");
-        self.sendSocketNotification("AUTH_TICKET_RESULT", { error: "Unable to retrieve authentication ticket." });
-        return;
-      }
-
-      console.log(`Got authentication ticket: ${authTicket}`);
-      self.authTicket = authTicket;
-
-      const cardAccountsOptions = {
-        method: "GET",
-        url: `${self.config.apiUrl}/user/cardaccounts`,
-        headers: {
-          "AuthenticationTicket": authTicket
+  makeRequest: function(options) {
+    var self = this;
+    request(options, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        const authTicket = response.headers["authenticationticket"];
+        console.log(response.headers); // Add this line
+        if (!authTicket) {
+          console.error("Error: Unable to retrieve authentication ticket.");
+          self.sendSocketNotification("AUTH_TICKET_RESULT", { error: "Unable to retrieve authentication ticket." });
+          return;
         }
-      };
 
-      self.makeCardAccountsRequest(cardAccountsOptions);
+        console.log(`Got authentication ticket: ${authTicket}`);
+        self.authTicket = authTicket;
 
-      // Schedule the next refresh 60 minutes after this request completes
-      setTimeout(() => {
-        console.log("Refreshing authentication ticket");
-        self.makeRequest(options);
-      }, 60 * 60 * 1000); // 60 minutes * 60 seconds * 1000 milliseconds
-    } else {
-      console.error(`Error getting authentication ticket: ${error}`);
-      self.sendSocketNotification("AUTH_TICKET_RESULT", { error: error });
-    }
-  });
-},
+        const cardAccountsOptions = {
+          method: "GET",
+          url: `${self.config.apiUrl}/user/cardaccounts`,
+          headers: {
+            "AuthenticationTicket": authTicket
+          }
+        };
+
+        self.makeCardAccountsRequest(cardAccountsOptions);
+      } else {
+        console.error(`Error getting authentication ticket: ${error}`);
+        self.sendSocketNotification("AUTH_TICKET_RESULT", { error: error });
+      }
+    });
+  },
+
+  makeCardAccountsRequest: function(options) {
+    var self = this;
+    request(options, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        const cardAccounts = JSON.parse(body);
+        console.log("Got card accounts:", cardAccounts);
+        self.sendSocketNotification("CARD_ACCOUNTS_RESULT", { cardAccounts: cardAccounts });
+        const favoriteStoresOptions = {
+          method: "GET",
+          url: `${self.config.storeApiUrl}/user/stores`,
+          headers: {
+            "AuthenticationTicket": self.authTicket
+          }
+        };
+        self.makeFavoriteStoresRequest(favoriteStoresOptions);
+      } else {
+        console.error(`Error getting card accounts: ${error}`);
+        self.sendSocketNotification("CARD_ACCOUNTS_RESULT", { error: error });
+      }
+    });
+  },
+
   makeFavoriteStoresRequest: function(options) {
     var self = this;
     request(options, function(error, response, body) {
