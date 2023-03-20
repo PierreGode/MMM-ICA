08b28178d8baa3778b3c9c952009586ab4f195be
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-03-20 09:18:04 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,5 +1,5 @@
 const NodeHelper = require("node_helper");
-const request = require("request");
+const axios = require("axios");
 
 module.exports = NodeHelper.create({
   start: function() {
@@ -25,98 +25,121 @@ module.exports = NodeHelper.create({
       this.makeRequest(options);
     }
   },
-makeRequest: function(options) {
-  var self = this;
-  request(options, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      const authTicket = response.headers["authenticationticket"];
-      console.log(response.headers); // Add this line
-      if (!authTicket) {
-        console.error("Error: Unable to retrieve authentication ticket.");
-        self.sendSocketNotification("AUTH_TICKET_RESULT", { error: "Unable to retrieve authentication ticket." });
-        return;
-      }
 
-      console.log(`Got authentication ticket: ${authTicket}`);
-      self.authTicket = authTicket;
-
-      // Call makeCardAccountsRequest immediately
-      self.makeCardAccountsRequest();
-
-      // Call makeCardAccountsRequest every minute
-      setInterval(() => {
-        self.makeCardAccountsRequest();
-      }, 60000);
-
-    } else {
-      console.error(`Error getting authentication ticket: ${error}`);
-      self.sendSocketNotification("AUTH_TICKET_RESULT", { error: error });
-    }
-  });
-},
-
-makeCardAccountsRequest: function(options) {
-    var self = this;
-    request(options, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-        const cardAccounts = JSON.parse(body);
-        console.log("Got card accounts:", cardAccounts);
-        self.sendSocketNotification("CARD_ACCOUNTS_RESULT", { cardAccounts: cardAccounts });
-        const favoriteStoresOptions = {
-          method: "GET",
-          url: `${self.config.storeApiUrl}/user/stores`,
-          headers: {
-            "AuthenticationTicket": self.authTicket
+  makeRequest: function(options) {
+    const self = this;
+    axios(options)
+      .then(function(response) {
+        if (response.status === 200) {
+          const authTicket = response.headers["authenticationticket"];
+          console.log(response.headers); // Add this line
+          if (!authTicket) {
+            console.error("Error: Unable to retrieve authentication ticket.");
+            self.sendSocketNotification("AUTH_TICKET_RESULT", { error: "Unable to retrieve authentication ticket." });
+            return;
           }
-        };
-        self.makeFavoriteStoresRequest(favoriteStoresOptions);
-      } else {
-        console.error(`Error getting card accounts: ${error}`);
-        self.sendSocketNotification("CARD_ACCOUNTS_RESULT", { error: error });
-      }
-    });
+
+          console.log(`Got authentication ticket: ${authTicket}`);
+          self.authTicket = authTicket;
+
+          const cardAccountsOptions = {
+            method: "GET",
+            url: `${self.config.apiUrl}/user/cardaccounts`,
+            headers: {
+              "AuthenticationTicket": authTicket
+            }
+          };
+
+          self.makeCardAccountsRequest(cardAccountsOptions);
+        } else {
+          console.error(`Error getting authentication ticket: ${response.statusText}`);
+          self.sendSocketNotification("AUTH_TICKET_RESULT", { error: response.statusText });
+        }
+      })
+      .catch(function(error) {
+        console.error(`Error getting authentication ticket: ${error}`);
+        self.sendSocketNotification("AUTH_TICKET_RESULT", { error: error.message });
+      });
   },
-makeOffersRequest: function(options) {
-  var self = this;
-  request(options, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      const offers = JSON.parse(body);
-      console.log("Got offers:", offers);
-      self.sendSocketNotification("OFFERS_RESULT", { offers: offers });
-    } else {
-      console.error(`Error getting offers: ${error}`);
-      self.sendSocketNotification("OFFERS_RESULT", { error: error });
-    }
-  });
-},
+
+  makeCardAccountsRequest: function(options) {
+    const self = this;
+    axios(options)
+      .then(function(response) {
+        if (response.status === 200) {
+          const cardAccounts = response.data;
+          console.log("Got card accounts:", cardAccounts);
+          self.sendSocketNotification("CARD_ACCOUNTS_RESULT", { cardAccounts: cardAccounts });
+          const favoriteStoresOptions = {
+            method: "GET",
+            url: `${self.config.storeApiUrl}/user/stores`,
+            headers: {
+              "AuthenticationTicket": self.authTicket
+            }
+          };
+          self.makeFavoriteStoresRequest(favoriteStoresOptions);
+        } else {
+          console.error(`Error getting card accounts: ${response.statusText}`);
+          self.sendSocketNotification("CARD_ACCOUNTS_RESULT", { error: response.statusText });
+        }
+      })
+      .catch(function(error) {
+        console.error(`Error getting card accounts: ${error}`);
+        self.sendSocketNotification("CARD_ACCOUNTS_RESULT", { error: error.message });
+      });
+  },
+
   makeFavoriteStoresRequest: function(options) {
-    var self = this;
-    request(options, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-        const favoriteStores = JSON.parse(body);
-        console.log("Got favorite stores:", favoriteStores);
-        self.sendSocketNotification("FAVORITE_STORES_RESULT", { favoriteStores: favoriteStores });
+    const self = this;
+    axios(options)
+      .then(function(response) {
+        if (response.status === 200) {
+          const favoriteStores = response.data;
+          console.log("Got favorite stores:", favoriteStores);
+          self.sendSocketNotification("FAVORITE_STORES_RESULT", { favoriteStores: favoriteStores });
 
-        const offersOptions = {
-          method: "GET",
-          url: `${self.config.storeApiUrl}/offers?Stores=${self.config.offersStoreId}`,
-          headers: {
-            "AuthenticationTicket": self.authTicket
-          }
+          const offersOptions = {
+            method: "GET",
+            url: `${self.config.storeApiUrl}/offers?Stores=${self.config.offersStoreId}`,
+            headers: {
+              "AuthenticationTicket": self.authTicket
+                        }
         };
-if (self.config.offersStoreId) {
-  const storeId = self.config.offersStoreId;
-  offersOptions.url = `${offersOptions.url}/store/${storeId}`;
-  console.log(`Retrieving offers for store ${storeId}`);
-} else {
-  console.log("Retrieving all offers");
-}
-
+        if (self.config.offers) {
+          const storeId = self.config.offers;
+          offersOptions.url = `${offersOptions.url}/store/${storeId}`;
+          console.log(`Retrieving offers for store ${storeId}`);
+        } else {
+          console.log("Retrieving all offers");
+        }
         self.makeOffersRequest(offersOptions);
       } else {
-        console.error(`Error getting favorite stores: ${error}`);
-        self.sendSocketNotification("FAVORITE_STORES_RESULT", { error: error });
+        console.error(`Error getting favorite stores: ${response.statusText}`);
+        self.sendSocketNotification("FAVORITE_STORES_RESULT", { error: response.statusText });
       }
-    });
+      })
+      .catch(function(error) {
+        console.error(`Error getting favorite stores: ${error}`);
+        self.sendSocketNotification("FAVORITE_STORES_RESULT", { error: error.message });
+      });
+  },
+
+  makeOffersRequest: function(options) {
+    const self = this;
+    axios(options)
+      .then(function(response) {
+        if (response.status === 200) {
+          const offers = response.data;
+          console.log("Got offers:", offers);
+          self.sendSocketNotification("OFFERS_RESULT", { offers: offers });
+        } else {
+          console.error(`Error getting offers: ${response.statusText}`);
+          self.sendSocketNotification("OFFERS_RESULT", { error: response.statusText });
+        }
+      })
+      .catch(function(error) {
+        console.error(`Error getting offers: ${error}`);
+        self.sendSocketNotification("OFFERS_RESULT", { error: error.message });
+      });
   }
 });
